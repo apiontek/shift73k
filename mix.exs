b0978d11b3ba4efeca2395b8ea87230070bc4765
Adam Piontek <adam@73k.us>
2021-03-05 12:59:11 -0500

    removing phx_gen_auth dep as no longer needed

--- mix.exs
+++ mix.exs
@@ -48,7 +48,6 @@ defmodule Bones73k.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phx_gen_auth, "~> 0.5", only: [:dev], runtime: false},
       {:ecto_enum, "~> 1.4"},
       {:bamboo, "~> 2.0"},
       {:bamboo_smtp, "~> 4.0"},
