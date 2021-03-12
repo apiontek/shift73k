b352416366594e446ba26b718b53316915a82001
Adam Piontek <adam@73k.us>
2021-03-12 18:44:25 -0500

    made bootstrap colors more flexible, styled calendar table better

--- lib/shift73k_web/live/shift_assign_live/index.ex
+++ lib/shift73k_web/live/shift_assign_live/index.ex
@@ -24,7 +24,6 @@ defmodule Shift73kWeb.ShiftAssignLive.Index do
     |> rotate_week()
     |> Keyword.values()
     |> Enum.map(&Timex.day_shortname/1)
-    |> IO.inspect(label: "day names...")
   end
 
   defp week_rows(cursor_date, week_start_at) do
@@ -37,12 +36,10 @@ defmodule Shift73kWeb.ShiftAssignLive.Index do
       cursor_date
       |> Timex.end_of_month()
       |> Timex.end_of_week(week_start_at)
-      |> IO.inspect(label: "last found")
 
     Interval.new(from: first, until: last, right_open: false)
     |> Enum.map(& &1)
     |> Enum.chunk_every(7)
-    |> IO.inspect(label: "week rows")
   end
 
   defp assign_day_names(socket) do
@@ -51,9 +48,7 @@ defmodule Shift73kWeb.ShiftAssignLive.Index do
   end
 
   defp assign_init_dates(socket, today) do
-    socket
-    |> assign(:current_date, today)
-    |> assign(:cursor_date, today)
+    assign(socket, [current_date: today, cursor_date: today])
   end
 
   defp assign_week_rows(socket) do
@@ -63,4 +58,25 @@ defmodule Shift73kWeb.ShiftAssignLive.Index do
     assign(socket, :week_rows, week_rows(cursor_date, week_start_at))
   end
 
+  def handle_event("prev-month", _, socket) do
+    cursor_date = Timex.shift(socket.assigns.cursor_date, months: -1)
+
+    assigns = [
+      cursor_date: cursor_date,
+      week_rows: week_rows(cursor_date, socket.assigns.current_user.week_start_at)
+    ]
+
+    {:noreply, assign(socket, assigns)}
+  end
+
+  def handle_event("next-month", _, socket) do
+    cursor_date = Timex.shift(socket.assigns.cursor_date, months: 1)
+
+    assigns = [
+      cursor_date: cursor_date,
+      week_rows: week_rows(cursor_date, socket.assigns.current_user.week_start_at)
+    ]
+
+    {:noreply, assign(socket, assigns)}
+  end
 end
